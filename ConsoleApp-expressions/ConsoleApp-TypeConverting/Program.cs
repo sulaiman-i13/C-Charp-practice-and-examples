f285a5e455e200263c7752fb54dc06509ab8b4de
sulaiman-13 <157150037+sulaiman-i13@users.noreply.github.com>
2024-02-08 20:39:41 +0300

    expressions

---
+++ ConsoleApp-expressions/ConsoleApp-TypeConverting/Program.cs
@@ -0,0 +1,41 @@
+﻿Console.WriteLine("==========BitConverter==============");
+
+int num = 4000;
+byte[] bits = BitConverter.GetBytes(num);
+foreach (var item in bits)
+{
+	Console.WriteLine(item);
+}
+Console.WriteLine("===========binary=============");
+foreach (var item in bits)
+{
+	Console.WriteLine(Convert.ToString(item, 2).PadLeft(8, '0'));
+}
+Console.WriteLine("===========CharArray=============");
+var name = "sulaiman";
+var nameArr = name.ToCharArray();
+foreach (var item in nameArr) Console.WriteLine(item);
+Console.WriteLine("===========ascii in decimal=============");
+
+foreach (var item in nameArr)
+	Console.WriteLine(Convert.ToInt32(item));
+Console.WriteLine("===========ascii in hex=============");
+
+foreach (var item in nameArr)
+	Console.WriteLine(Convert.ToString(Convert.ToInt32(item),16));
+Console.WriteLine("===========ascii to char=============");
+var ascii = "73,75,6c,61,69,6d,61,6e".Split(",");
+foreach(var item in ascii)
+{
+	var i = Convert.ToInt32(item, 16);
+    Console.WriteLine((char)i);
+}
+Console.WriteLine("===========hex to decimal=============");
+var _hex1 = "b82";
+var _hex2 = "ff";
+int dec1 = Convert.ToInt32(_hex1, 16);
+int dec2 = Convert.ToInt32(_hex2, 16);
+int dec3 = int.Parse(_hex2, System.Globalization.NumberStyles.HexNumber);
+Console.WriteLine(dec1);
+Console.WriteLine(dec2);
+Console.WriteLine(dec3);
---
+++ ConsoleApp-expressions/ConsoleApp-expressions/Program.cs
@@ -0,0 +1,67 @@
+﻿using System.Diagnostics.Metrics;
+
+Console.WriteLine("==============switch===================");
+var name = "sulaiman";
+switch (name)
+{
+	case "sulaiman": Console.WriteLine("<=s="); break;
+	case "Ibrahem": Console.WriteLine("=i="); break;
+	case "Ismaeel": Console.WriteLine("=i=>"); break;
+	default: Console.WriteLine("no name"); break;
+}
+Console.WriteLine("==============switch on type===================");
+object o = 1;
+switch (o)
+{
+	case int:
+		Console.WriteLine("its int");
+		break;
+	case float:
+		Console.WriteLine("its float");
+		break;
+	case string:
+		Console.WriteLine("its string");
+		break;
+
+}
+Console.WriteLine("==============switch predicate ===================");
+
+bool b = true;
+switch (b)
+{
+	case bool i when i==true:
+		Console.WriteLine("its true");
+		break;
+	case bool i when i==false:
+		Console.WriteLine("its false");
+		break;
+
+
+}
+Console.WriteLine("==============switch expression ===================");
+var cardNumber = 7;
+var cardName = cardNumber switch
+{
+	1 => "Ase",
+	11 => "Jack",
+	12 => "Queen",
+	13 => "King",
+	_=>cardNumber.ToString()
+};
+Console.WriteLine(cardName);
+
+
+Console.WriteLine("==============fibonacci ===================");
+
+for (int counter=0,prev = 0, current = 1;counter < 10; counter++ ){
+
+	int newNumber = prev + current;
+	prev = current;
+	current = newNumber;
+    Console.WriteLine(	$"Current: { current}");
+}
+Console.WriteLine("==============Odd Numbers ===================");
+for(int i = 0; i < 10; i++)
+{
+	if (i % 2 != 0) { Console.WriteLine($"i: {i}"); }
+}

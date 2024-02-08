b42f7991d10b59a427836282a6db00c381c829bf
sulaiman-13 <157150037+sulaiman-i13@users.noreply.github.com>
2024-02-08 20:41:23 +0300

    exceptions

---
+++ ConsoleApp-Exceptions/ConsoleApp-Exceptions/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+try
+{
+    //var x = 10;
+    string? y = null;
+	//var z = 0;
+	//var w = x / z;
+	//Console.WriteLine(y.ToUpper());
+	throw new MyDivideByZeroException("this is my exception thrown");
+
+	
+
+
+}
+catch(MyDivideByZeroException e) { 
+    Console.WriteLine(e.Message); 
+}
+//we can use filter on catching an exception like this:
+
+catch (Exception e)when (e.Source == "ConsoleApp-Exceptions")
+{
+    
+    Console.WriteLine(e.Message);
+}
+finally
+{
+    Console.WriteLine("finished");
+}
+
+class MyDivideByZeroException : DivideByZeroException
+{
+    public MyDivideByZeroException(string message):base(message)
+    {
+        
+    }
+}

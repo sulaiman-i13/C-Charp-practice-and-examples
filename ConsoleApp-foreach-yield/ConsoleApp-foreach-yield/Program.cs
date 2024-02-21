fe92eba7b0f80dc0f372437fdc5b612818d24074
sulaiman-13 <157150037+sulaiman-i13@users.noreply.github.com>
2024-02-22 00:14:27 +0300

    foreach

---
+++ ConsoleApp-foreach-yield/ConsoleApp-foreach-yield/Program.cs
@@ -0,0 +1,26 @@
+﻿#region foreach
+static void ForEach<T>( IEnumerable<T> sequence)
+{
+	IEnumerator<T> enumerator = sequence.GetEnumerator();
+	IDisposable disposable = enumerator as IDisposable;
+	try
+	{
+		while (enumerator.MoveNext())
+		{
+			
+            Console.WriteLine(enumerator.Current);
+        }
+	}
+	catch (Exception)
+	{
+
+		enumerator.Reset();
+	}
+	finally { disposable.Dispose(); }
+}
+var seq1 = new List<int> { 1, 2, 3, 3, 44, 4, 5, 5, 6 };
+var seq2 = new List<string> { "sulaiman", "ibrahem", "ismaeel" };
+ForEach(seq1);
+ForEach(seq2);
+
+#endregion
